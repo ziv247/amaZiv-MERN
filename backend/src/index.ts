7a757a60f601a342e9cb13748861cbdd8fa2239c
ZIV-LAPTOP\Ziv <ziv@allseated.com>
2023-05-21 18:08:56 +0300

    connecting mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,21 @@
 import cors from "cors";
-import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import { productRouter } from "./routers/productRouter";
+import { seedRouter } from "./routers/seedRouter";
 
-import { sampleProducts } from "./data";
-
+dotenv.config();
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/tsamazivdb";
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch(() => {
+    console.log("error mongodb");
+  });
 const app = express();
 app.use(
   cors({
@@ -11,13 +24,8 @@ app.use(
   })
 );
 
-app.get("/api/products", (req: Request, res: Response) => {
-  res.json(sampleProducts);
-});
-
-app.get("/api/products/:slug", (req: Request, res: Response) => {
-  res.json(sampleProducts.find((prod) => prod.slug === req.params.slug));
-});
+app.use("/api/products", productRouter);
+app.use("/api/seed", seedRouter);
 
 const PORT = 4000;
 app.listen(PORT, () => {
